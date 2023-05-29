d08e485b39993a1f06e8493f455843fbdf5834ce
轩哥啊哈OvO <108124782+Xuangeaha@users.noreply.github.com>
2023-05-29 21:22:26 +0800

    2023 更美观的代码

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
 import time
 import json
 import requests
-
 from win10toast import ToastNotifier
-t = ToastNotifier()
 
-rp = requests.get("https://api.bilibili.com/x/relation/stat?vmid=1865984682")
-jsd = json.loads(rp.text)
-flr = jsd['data']['follower']
-flrnw = int(flr)
+def _get_follower(url: str):
+    return int(json.loads(requests.get(url).text)['data']['follower'])
 
-rp1 = requests.get("https://api.bilibili.com/x/space/top/arc?vmid=1865984682&jsonp=jsonp")
-jsd1 = json.loads(rp1.text)
-flr1 = jsd1['data']['stat']['view']
-like = jsd1['data']['stat']['like']
-likecot = int(like)
-cot = int(flr1)
-ct = 0
+def _get_top_artivle_view(url: str):
+    return int(json.loads(requests.get(url).text)['data']['stat']['view'])
 
-haha = 0
-while haha == 0:
-        print("刷新..")
-        ct += 1
-        rp = requests.get("https://api.bilibili.com/x/relation/stat?vmid=1865984682")
-        jsd = json.loads(rp.text)
-        flr = jsd['data']['follower']
+def _get_top_artivle_like(url: str):
+    return int(json.loads(requests.get(url).text)['data']['stat']['like'])
 
-        rp1 = requests.get("https://api.bilibili.com/x/space/top/arc?vmid=1865984682&jsonp=jsonp")
-        jsd1 = json.loads(rp1.text)
-        flr1 = jsd1['data']['stat']['view']
-        like = jsd1['data']['stat']['like']
+_follower = _get_follower("https://api.bilibili.com/x/relation/stat?vmid=1865984682")
+_top_artivle_view = _get_top_artivle_view("https://api.bilibili.com/x/space/top/arc?vmid=1865984682")
+_top_artivle_like = _get_top_artivle_like("https://api.bilibili.com/x/space/top/arc?vmid=1865984682")
 
-        if int(flr) > int(flrnw):
-                ti = "恭喜你，你新增了 " + str(int(flr) - int(flrnw)) + " 个粉丝！"
-                rule = "你现在的粉丝量为：" + str(flr)
-                t.show_toast(title=ti, msg=rule, duration=15)
-                flrnw = int(flr)
-        elif int(flr) < int(flrnw):
-                ti = "啊哦，你掉了 " + str(int(flrnw) - int(flr)) + " 个粉丝！ "
-                rule = "你现在的粉丝量为：" + str(flr)
-                t.show_toast(title=ti, msg=rule,
-                        duration=5)
-                flrnw = int(flr)
+while True:
+    print("刷新...")
 
-        if int(flr1) > int(cot):
-                ti = "恭喜你，你最近一期视频的播放量新增了 " + str(int(flr1) - int(cot))
-                rule = "现在的播放量为：" + str(flr1)
-                t.show_toast(title=ti, msg=rule, duration=15)
-                cot = int(flr1)
+    follower = _get_follower("https://api.bilibili.com/x/relation/stat?vmid=1865984682")
+    top_artivle_view = _get_top_artivle_view("https://api.bilibili.com/x/space/top/arc?vmid=1865984682")
+    top_artivle_like = _get_top_artivle_like("https://api.bilibili.com/x/space/top/arc?vmid=1865984682")
 
-        if int(like) > int(likecot):
-                ti = "恭喜你，你最近一期视频的点赞数新增了 " + str(int(like) - int(likecot))
-                rule = "现在的点赞数为：" + str(like)
-                t.show_toast(title=ti, msg=rule, duration=15)
-                likecot = int(like)
-                
-        time.sleep(60)
+    if follower > _follower:
+        _title = "恭喜你，你新增了 " + str(follower - _follower) + " 个粉丝！"
+        _msg = "你现在的粉丝量为：" + str(follower)
+        ToastNotifier.show_toast(title=_title, msg=_msg, duration=15)
+        _follower = follower
+    elif follower < _follower:
+        _title = "啊哦，你掉了 " + str(_follower - follower) + " 个粉丝！ "
+        _msg = "你现在的粉丝量为：" + str(follower)
+        ToastNotifier.show_toast(title=_title, msg=_msg, duration=5)
+        _follower = follower
 
-        
+    if top_artivle_view > _top_artivle_view:
+        _title = "恭喜你，你最近一期视频的播放量新增了 " + str(top_artivle_view - _top_artivle_view)
+        _msg = "现在的播放量为：" + str(top_artivle_view)
+        ToastNotifier.show_toast(title=_title, msg=_msg, duration=15)
+        _top_artivle_view = top_artivle_view
+
+    if top_artivle_like > _top_artivle_like:
+        _title = "恭喜你，你最近一期视频的点赞数新增了 " + str(top_artivle_like - _top_artivle_like)
+        _msg = "现在的点赞数为：" + str(top_artivle_like)
+        ToastNotifier.show_toast(title=_title, msg=_msg, duration=15)
+        _top_artivle_like = top_artivle_like
+            
+    time.sleep(60)
+
+    
